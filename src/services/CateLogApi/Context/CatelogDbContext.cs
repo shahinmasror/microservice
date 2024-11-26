743943d39163e3c663447abad2752816611f2ee1
shahin <shahinmasror.tb@gmail.com>
2024-11-26 22:51:41 +0600

    add CatalogdbContext

---
+++ src/services/CateLogApi/Context/CatelogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoRepo.Context;
+
+namespace CateLogApi.Context
+{
+    public class CatelogDbContext : ApplicationDbContext
+    {
+        static IConfiguration configureation = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
+        static string connectionString = configureation.GetConnectionString("CateLog.Api");
+        static string databaseName = configureation.GetConnectionString("DatabaseName");
+        public CatelogDbContext() : base(connectionString, databaseName)
+        {
+        }
+    }
+}
